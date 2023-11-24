ba283d41df491c7f2b2d060fb970360b1a73a7ea
Sam Guzel <Samblues@users.noreply.github.com>
2023-11-24 00:12:15 +0000

    feat: boiler plate down for homePage, sideNav and auth

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+function Head() {
+  return (
+    <>
+    <title>SamGPT Project</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport"></meta>
+    <link rel="icon" href="/favicon.ico"></link>
+    </>
+  )
+}
+export default Head
